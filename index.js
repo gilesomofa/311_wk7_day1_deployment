273d315b32ec7c59a8f09bf2ee0b7e02e132b576
Your Name <you@example.com>
2019-12-16 20:23:18 -0600

    got it working

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use('/users', usersRouter)
 app.use('/auth', authRouter)
 
 app.get('/', (req, res) => {
-  res.send('Welcome to our server!')
+  res.send('Welcome to our new server!')
 })
 
 app.listen(port, () => {
